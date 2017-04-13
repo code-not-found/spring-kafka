5ed6dc13c07bed0046798d110aca3b8957aba3e0
codenotfound.com <codenotfound.com@gmail.com>
2017-04-13 20:45:24 +0200

    Adding spring-kafka-boot

---
+++ spring-kafka-boot/src/main/java/com/codenotfound/kafka/consumer/Receiver.java
@@ -0,0 +1,27 @@
+package com.codenotfound.kafka.consumer;
+
+import java.util.concurrent.CountDownLatch;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Receiver {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(Receiver.class);
+
+  private CountDownLatch latch = new CountDownLatch(1);
+
+  public CountDownLatch getLatch() {
+    return latch;
+  }
+
+  @KafkaListener(topics = "${topic.boot}")
+  public void receive(ConsumerRecord<?, ?> consumerRecord) {
+    LOGGER.info("received data='{}'", consumerRecord.toString());
+    latch.countDown();
+  }
+}
---
+++ spring-kafka-boot/src/test/java/com/codenotfound/kafka/SpringKafkaApplicationTest.java
@@ -0,0 +1,46 @@
+package com.codenotfound.kafka;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.rule.KafkaEmbedded;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.codenotfound.kafka.consumer.Receiver;
+import com.codenotfound.kafka.producer.Sender;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringKafkaApplicationTest {
+
+  private static String BOOT_TOPIC = "boot.t";
+
+  @Autowired
+  private Sender sender;
+
+  @Autowired
+  private Receiver receiver;
+
+  @ClassRule
+  public static KafkaEmbedded embeddedKafka = new KafkaEmbedded(1, true, BOOT_TOPIC);
+
+  @BeforeClass
+  public static void setUpBeforeClass() throws Exception {
+    System.setProperty("spring.kafka.bootstrap-servers", embeddedKafka.getBrokersAsString());
+  }
+
+  @Test
+  public void testReceive() throws Exception {
+    sender.send(BOOT_TOPIC, "Hello Boot!");
+
+    receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
+    assertThat(receiver.getLatch().getCount()).isEqualTo(0);
+  }
+}
